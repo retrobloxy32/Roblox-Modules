fda0c163c4a1d91f0ac8abe90a6137d202bdc386
RetroBloxy <karimlachgar32@gmail.com>
2025-05-11 17:05:35 +0100

    Add files via upload

--- TimerModule/TimerModule/TimerModule.lua
+++ TimerModule/TimerModule/TimerModule.lua
@@ -13,14 +13,15 @@ local Timers = {}
 
 function Timer.Create(Duration: number, Name: string?)
 	local self = setmetatable({}, Index)
+	
+	if Name == nil then
+		Name = "Nameless Timer " .. NamelessTimers
+		NamelessTimers += 1
+	end
+	
 	self.StartTime = Duration
 	self.Time = Duration
-	self.Name = Name or function()
-		local TimerName = "Nameless Timer" .. NamelessTimers
-		NamelessTimers += 1
-
-		return TimerName
-	end
+	self.Name = Name 
 
 	EndEvents[self.Name] = Instance.new("BindableEvent")
 	UpdateEvents[self.Name] = Instance.new("BindableEvent")
@@ -52,21 +53,22 @@ function Meta:Start()
 	Timers[self.Name] = coroutine.create(function()
 		for i = self.StartTime, 0, -1 do
 			if not TimerStatus[self.Name].Running then break end
-
+			
 			while TimerStatus[self.Name].Paused and TimerStatus[self.Name].Running do
 				task.wait()
 			end
-
+			
 			if not TimerStatus[self.Name].Running then break end
-
+			
 			self.Time = i
 			UpdateEvents[self.Name]:Fire(i)
 			task.wait(1)
 		end
-
+		
+		if not TimerStatus[self.Name].Running then return end
 		self:Stop()
 	end)
-
+	
 	coroutine.resume(Timers[self.Name])
 end
 
@@ -100,7 +102,7 @@ end
 function Meta:Stop()
 	if not Timers[self.Name] then return warn(`no timer for "{self.Name}"`) end
 	print(`Stopping "{self.Name}"`)
-
+	
 	TimerStatus[self.Name].Running = false
 	EndEvents[self.Name]:Fire()
 
